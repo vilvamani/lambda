b4b7f6fd316e17032a0f29f23c8105b8ddd8a16f
vilvamani <41226243+vilvamani@users.noreply.github.com>
2025-03-04 21:47:16 +0530

    Update lambda_function-3.py

--- lambda_function-3.py
+++ lambda_function-3.py
@@ -337,7 +337,7 @@ def get_ecs_cluster():
     for cluster_arn in response:
         cluster = ecs.describe_clusters(clusters=[cluster_arn], include=['TAGS'])['clusters']
         
-        if not cluster['tags']:
+        if 'tags 'not in cluster:
                 data.append([cluster['clusterName'], "Resource_Not_Tagged"])
 
         else:
